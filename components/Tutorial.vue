b0278042ad94424444a3ba1a72ad547b9ebf544c
Sraddha--aa <sraddha@mozilor.com>
2023-08-22 12:13:10 +0530

    new project

---
+++ components/Tutorial.vue
@@ -0,0 +1,17 @@
+<!-- components/SimpleComponent.vue -->
+<template>
+  <div>
+    <h1>Hello from Nuxt.js</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'This is a Nuxt.js component.'
+    };
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<!-- pages/index.vue -->
+<template>
+  <div>
+    <SimpleComponent />
+  </div>
+</template>
+
+<script>
+import SimpleComponent from '~/components/SimpleComponent.vue';
+
+export default {
+  components: {
+    SimpleComponent
+  }
+};
+</script>
